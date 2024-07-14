1b54ee448a869d580de73ead9f498f7fb0f90300
ikechkwukelvin <chichi.ikechukwu@yahoo.com>
2024-07-14 01:40:11 +0100

    updated

--- scripts/place.js
+++ scripts/place.js
@@ -13,23 +13,22 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const windChill = calculateWindChill(temperature, windSpeed);
     windChillElement.textContent = (windChill !== 'N/A') ? `${windChill} °C` : 'N/A';
-
-    // Function to get current year and set it in the span element
-    function setCurrentYear() {
-        const currentYear = new Date().getFullYear();
-        document.getElementById("currentyear").textContent = currentYear;
-    }
-
-    // Function to get last modified date and set it in the paragraph element
-    function setLastModified() {
-        const lastModified = document.lastModified;
-        document.getElementById("lastModified").textContent = "Last modified: " + lastModified;
-    }
-
-    // Call the functions to set current year and last modified date when the page loads
-    window.onload = function() {
-        setCurrentYear();
-        setLastModified();
-    };
 });
 
+// Function to get current year and set it in the span element
+function setCurrentYear() {
+    const currentYear = new Date().getFullYear();
+    document.getElementById("currentyear").textContent = currentYear;
+}
+
+// Function to get last modified date and set it in the paragraph element
+function setLastModified() {
+    const lastModified = document.lastModified;
+    document.getElementById("lastModified").textContent = "Last modified: " + lastModified;
+}
+
+// Call the functions to set current year and last modified date when the page loads
+window.onload = function() {
+    setCurrentYear();
+    setLastModified();
+};
